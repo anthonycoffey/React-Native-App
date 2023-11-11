e508cb76114b307519e0963af01bd47701828043
Anthony Coffey <coffey.j.anthony@gmail.com>
2023-11-11 03:54:16 -0600

    minor improvements to latest auth changes

--- ctx.tsx
+++ ctx.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useStorageState } from "./useStorageState";
 import api from "@/utils/api";
-import { router } from "expo-router";
 
 const AuthContext = React.createContext<{
   signIn: () => void;
@@ -28,24 +27,12 @@ export function SessionProvider(props: React.PropsWithChildren) {
   return (
     <AuthContext.Provider
       value={{
-        signIn: (token) => {
-          // Perform sign-in logic here
-          setSession("token", token);
-          // Add a request interceptor
-          api.interceptors.request.use(
-            (config) => {
-              // Do something before request is sent
-              if (token) {
-                config.headers.Authorization = `Bearer ${token}`;
-              }
-              return config;
-            },
-            (error) => {
-              // Do something with request error
-              return Promise.reject(error);
-            },
-          );
-          router.push("(app)");
+        signIn: (token: string) => {
+          setSession(token);
+          api.interceptors.request.use((config: any) => {
+            config.headers.Authorization = `Bearer ${token}`;
+            return config;
+          });
         },
         signOut: () => {
           setSession(null);
