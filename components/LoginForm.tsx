f4fb7ae6e81add016999ae62f539794ce6803a03
Anthony Coffey <coffey.j.anthony@gmail.com>
2023-11-29 11:55:31 -0600

    removing test data out of login form component

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -9,8 +9,8 @@ import { Eye, EyeOff } from "@tamagui/lucide-icons";
 
 export default function LoginForm() {
   const { signIn } = useSession();
-  const [email, setEmail] = useState("tech@test.com");
-  const [password, setPassword] = useState("test1234");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isVisible, setIsVisible] = useState(false);
 
   const toggleVisibility = () => {
