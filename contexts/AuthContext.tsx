cc229bf7a49784d70853cde7bde574212d8dae4a
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-07-05 16:49:37 -0500

    adding sentry and support to edit user info

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -14,6 +14,7 @@ type AuthContextType = {
   session: string | null;
   isLoading: boolean;
   currentUser: User | null;
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | null>>;
   isUserLoading: boolean;
   fetchCurrentUser: () => Promise<void>;
   isApiAuthReady: boolean;
@@ -124,6 +125,7 @@ export function AuthProvider(props: React.PropsWithChildren) {
     session,
     isLoading: isLoadingSession,
     currentUser,
+    setCurrentUser,
     isUserLoading,
     fetchCurrentUser: () => fetchAndSetCurrentUser(session),
     isApiAuthReady,
